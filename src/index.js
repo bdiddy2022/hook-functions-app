72100d06be84a389c85c9682bdb0d00b0aff78fb
bdiddy2022 <brendan.t.downs@gmail.com>
2023-01-31 16:27:55 -0500

    created clickable star rating function

--- src/index.js
+++ src/index.js
@@ -4,18 +4,32 @@ import { isCompositeComponent } from 'react-dom/test-utils';
 import './index.css';
 import { FaStar } from 'react-icons/fa';
 
+
+
 const createArray = (length) => [
   ...Array(length)
 ];
 
-function StarRating({totalStars =5}) {
-  return createArray(totalStars).map((n, i) => (
-    <Star key={i} />
-  ));
+function StarRating({ totalStars = 5 }) {
+
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+    {createArray(totalStars).map((n, i) => (
+      <Star
+        key={i}
+        selected={selectedStars > i}
+        onSelect={() => setSelectedStars(i + 1)}
+      />
+      ))}
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
 }
 
-function Star({selected=false}) {
-  return (<FaStar color={selected ? "red" : "gray"}/>);
+function Star({ selected = false, onSelect }) {
+  return (<FaStar onClick={onSelect} color={selected ? "red" : "gray"} />);
 }
 
 function App() {
